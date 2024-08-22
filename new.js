69561acb2af46d37dc32776e9fdba27c9fc282c4
Jeeva <144088242+MJeevaCSE@users.noreply.github.com>
2024-08-22 22:29:21 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,31 @@
+let a=Math.floor(Math.random()*100);
+let count=1;
+function check(){
+   let b=document.getElementById("no"); 
+
+
+if(b.value==a&&count<=10){
+    document.getElementById("sentence").innerHTML="Found correctly!!!";
+   window.location.href="./win.html";   
+}
+else  if(b.value>=a&&count<=10){
+
+       document.getElementById("sentence").innerHTML="Guess min value";
+       document.getElementById("number").innerHTML=count;
+       count++;
+    }
+else  if(b.value<=a&&count<=10){
+
+        document.getElementById("sentence").innerHTML="Guess max value";
+        document.getElementById("number").innerHTML=count;
+        count++;
+     }
+else if(b.value!=a&&count>10){
+    document.getElementById("sentence").innerHTML="Attempts finished";
+    window.location.href="./loss.html";
+}
+    
+   }
+function reset(){
+   location.reload();
+}
