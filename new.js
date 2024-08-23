35ef2542ee5fcee08772a56f4a9d8452908f9284
Jeeva <144088242+MJeevaCSE@users.noreply.github.com>
2024-08-23 09:47:23 +0530

    new.js

--- new.js
+++ new.js
@@ -8,13 +8,13 @@ if(b.value==a&&count<=10){
     document.getElementById("sentence").innerHTML="Found correctly!!!";
    window.location.href="./win.html";   
 }
-else  if(b.value>=a&&count<=10){
+else  if(b.value>a&&count<=10){
 
        document.getElementById("sentence").innerHTML="Guess min value";
        document.getElementById("number").innerHTML=count;
        count++;
     }
-else  if(b.value<=a&&count<=10){
+else  if(b.value<a&&count<=10){
 
         document.getElementById("sentence").innerHTML="Guess max value";
         document.getElementById("number").innerHTML=count;
